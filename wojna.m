efff51a92b570c841071c4d333f88ad593aaa768
ROBIAGA <56342391+ROBIAGA@users.noreply.github.com>
2019-10-09 11:42:14 +0200

    robert
    start

---
+++ wojna.m
@@ -0,0 +1,47 @@
+clc;
+clear;
+close all;
+a=(1:13);
+b=(1:13);
+c=(1:13);
+d=(1:13);
+e=[a b c d];
+e=e(randperm(length(e)));   %tasowanie
+talia1=e(1:26)              %rozdanie
+talia2=e(27:52)
+n= 1000
+for i= 1:n
+    ruch1=talia1(1)
+    ruch2=talia2(1)
+    
+    if ruch1>ruch2
+        talia1=[talia1 ruch2]
+        talia2=[talia2-ruch2]
+        ruch1=talia1(2)
+        ruch2=talia2(2)
+    elseif ruch1<ruch2
+        talia1=[talia1-ruch1]
+        talia2=[talia2 ruch1]
+        ruch1=talia1(2)
+        ruch2=talia2(2)
+    elseif ruch1==ruch2
+        ruch1=talia1(3)
+        ruch2=talia2(3)
+        if ruch1>ruch2
+            talia1=[talia1 ruch2]
+            talia2=[talia2-ruch2]
+            ruch1=talia1(2)
+            ruch2=talia2(2)
+        elseif ruch1<ruch2
+            talia1=[talia1-ruch1]
+            talia2=[talia2 ruch1]
+            ruch1=talia1(2)
+            ruch2=talia2(2)
+        else
+        end
+    else 
+        disp('koniec gry')
+        
+    end
+end
+    
