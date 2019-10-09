5f1c42169218d4c8ff8b2a3c80ffefbd4fc18cfc
msadek95 <56342302+msadek95@users.noreply.github.com>
2019-10-09 12:08:36 +0200

    Add files via upload
    wrzucam update

--- wojna.m
+++ wojna.m
@@ -10,34 +10,25 @@ e=e(randperm(length(e)));   %tasowanie
 talia1=e(1:26)              %rozdanie
 talia2=e(27:52)
 n= 1000
-for i= 1:n
+while isempty(talia1)==0 & isempty(talia2)==0
     ruch1=talia1(1)
     ruch2=talia2(1)
+    talia1=talia1(2:length(talia1))
+    talia2=talia2(2:length(talia2))
     
-    if ruch1>ruch2
-        talia1=[talia1 ruch2]
-        talia2=[talia2-ruch2]
-        ruch1=talia1(2)
-        ruch2=talia2(2)
-    elseif ruch1<ruch2
-        talia1=[talia1-ruch1]
-        talia2=[talia2 ruch1]
-        ruch1=talia1(2)
-        ruch2=talia2(2)
-    elseif ruch1==ruch2
-        ruch1=talia1(3)
-        ruch2=talia2(3)
-        if ruch1>ruch2
-            talia1=[talia1 ruch2]
-            talia2=[talia2-ruch2]
-            ruch1=talia1(2)
-            ruch2=talia2(2)
-        elseif ruch1<ruch2
-            talia1=[talia1-ruch1]
-            talia2=[talia2 ruch1]
-            ruch1=talia1(2)
-            ruch2=talia2(2)
-        else
+    if ruch1(1)>ruch2(1)
+        talia1=[talia1 ruch1 ruch2]
+       
+       
+    elseif ruch1(1)<ruch2(1)
+        talia2=[talia2 ruch1 ruch2]
+       
+    elseif ruch1(1)==ruch2(1)
+ruch1=[talia1(2) talia1(1) ruch1]
+ruch2=[talia2(2) talia2(1) ruch1]
+talia1=talia1(3:length(talia1))
+talia2=talia2(3:length(talia2))
+
         end
     else 
         disp('koniec gry')
